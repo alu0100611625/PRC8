b0d759c7ab7ce0e924b80fe8cb1417d9469095e4
Sergio Martín <alu0100611625@ull.edu.es>
2013-11-04 19:51:27 +0000

    Solucionadas las dos primeras espectativas. Se crea la instancia a Matriz y se suman dos Matrices

--- lib/matriz.rb
+++ lib/matriz.rb
@@ -1,1 +1,21 @@
 #Archivo que contiene el codigo"
+class Matriz
+ attr_reader :m, :tamano
+
+        def initialize (una_matriz)
+                @m = una_matriz
+                @tamano = una_matriz.size-1
+        end
+        
+
+
+        def + (obj)
+                i = j = 0
+                for i in 0..@tamano do
+                        for j in 0..@tamano do
+                                obj.m[i][j] = @m[i][j] + obj.m[i][j]
+                        end
+                end
+        end
+
+end
--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -3,7 +3,7 @@ require 'matriz'
 describe Matriz do
 	before :each do
 		@matriz = Matriz.new([[1,2],[3,4]])
-
+	end
 
 	it "Comprobando que se suman dos matrices" do
 		@matriz + Matriz.new([[1,2],[0,1]])
