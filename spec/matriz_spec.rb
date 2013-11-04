4f730228e9339ce5623f579b1c39d4435ea0a8a3
alu0100536690 <alu0100536690@ull.edu.es>
2013-11-04 19:58:21 +0000

    Añadiendo espectativa metodo multiplicar

--- spec/matriz_spec.rb
+++ spec/matriz_spec.rb
@@ -3,10 +3,15 @@ require 'matriz'
 describe Matriz do
 	before :each do
 		@matriz = Matriz.new([[1,2],[3,4]])
-	end
+
 
 	it "Comprobando que se suman dos matrices" do
 		@matriz + Matriz.new([[1,2],[0,1]])
+	end
+
+
+	it "Comprobado que se multiplica matriz" do
+		@matriz * Matriz.new([[1,2],[4,2]])
 	end
 
 end
